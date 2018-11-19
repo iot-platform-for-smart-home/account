4d0bd8a3fc62f7b31014ed2233386131acf93f90
wizardmanxxx <wxd5566@qq.com>
2018-11-19 19:56:19 +0800

    补充同步文件

---
+++ src/main/java/com/edu/bupt/new_account/dao/RelationMapper.java
@@ -0,0 +1,25 @@
+package com.edu.bupt.new_account.dao;
+
+import com.edu.bupt.new_account.model.Relation;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface RelationMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Relation record);
+
+    int insertSelective(Relation record);
+
+    Relation selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Relation record);
+
+    int updateByPrimaryKey(Relation record);
+
+    List<Relation> getBindedRelations(int bindedId);
+
+    Relation getRelationBy2Bind(Relation re);
+}
---
+++ src/main/java/com/edu/bupt/new_account/model/Relation.java
@@ -0,0 +1,54 @@
+package com.edu.bupt.new_account.model;
+
+public class Relation {
+    private Integer id;
+
+    private Integer binder;
+
+    private Integer binded;
+
+    private String gateid;
+
+    public Relation(Integer id, Integer binder, Integer binded, String gateid) {
+        this.id = id;
+        this.binder = binder;
+        this.binded = binded;
+        this.gateid = gateid;
+    }
+
+    public Relation() {
+        super();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getBinder() {
+        return binder;
+    }
+
+    public void setBinder(Integer binder) {
+        this.binder = binder;
+    }
+
+    public Integer getBinded() {
+        return binded;
+    }
+
+    public void setBinded(Integer binded) {
+        this.binded = binded;
+    }
+
+    public String getGateid() {
+        return gateid;
+    }
+
+    public void setGateid(String gateid) {
+        this.gateid = gateid == null ? null : gateid.trim();
+    }
+}
